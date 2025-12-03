44ba6ad50b08a8c9739964c8e33ea1e74af9c618
BahaBlastTG <nuhuhcasio@gmail.com>
2025-12-03 19:29:44 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,12 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
